c8c9febd879034e3a57b0ebefe758269f7333805
Meghan Palagyi <meghanp@stanford.edu>
2013-04-18 10:46:12 -0700

    Combine landing and standard page 1 layouts.

--- layouts/gsb-landing_standard-page-one/gsb-landing_standard-page-one.tpl.php
+++ layouts/gsb-landing_standard-page-one/gsb-landing_standard-page-one.tpl.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Template for Panopoly Selby Flipped.
+ * Template for GSB Landing Standard Page 1.
  *
  * Variables:
  * - $css_id: An optional CSS id to use for the layout.
@@ -10,7 +10,7 @@
  */
 ?>
 
-<div class="panel-display gsb-landing-page-one <?php if (!empty($class)) { print $class; } ?>">
+<div class="panel-display gsb-landing_standard-page-one <?php if (!empty($class)) { print $class; } ?>">
 
   <div class="full-header">
     <?php print $content['fullheader']; ?>
