b9f93b2b070e535590eda94f4b8c9897734e3981
Meghan Palagyi <meghanp@stanford.edu>
2013-04-19 16:37:53 -0700

    Business insights, add alex and kirills work.

--- js/custom.js
+++ js/custom.js
@@ -200,7 +200,49 @@
             numTweets: _length,
             appendTo: '.pane-bundle-social-feed'
           });
-        } 
+        }
+      }
+    }
+  }
+
+  /**
+   * Add highlighted map functionality for BI sidebar menu
+   */
+  Drupal.behaviors.map_hover = {
+    attach: function (context, settings) {
+      if ($('.bi-map').length) {
+        var sidebar = $('.gsb-bizin-sidebar'),
+          biMenu = sidebar.find('.view-business-insights-sidebar'),
+          biMap = sidebar.find('.bi-map'),
+          biMaptext = biMap.find('.bi-map__text'),
+          biMapOverlay = sidebar.find('.bi-map__overlay');
+
+        /* if js is applied show map. */
+        biMap.show();
+        /* hide text block until user hovers or link is active. */
+        biMaptext.hide();
+
+        /* if location term page is opened, highlight the map. */
+        var locationTerms = biMenu.find('.menu_region').find('a.active');
+        if  (locationTerms.length) {
+          change_map(locationTerms.text());
+        }
+
+        /* change map image on region hover */
+        $('.bi-map-area').find('area').mouseover(function() {change_map($(this).attr('alt'));})
+          .mouseout(function() {revert_map();});
+      }
+
+      /* change map background on hover */
+      function change_map(t) {
+        biMapOverlay.removeClass().addClass('bi-map__overlay ' + t.replace(/ /g, '').toLowerCase());
+        biMaptext.show().text(t);
+      }
+
+      /* return default map background */
+      function revert_map(t) {
+        biMapOverlay.removeClass();
+        biMaptext.text();
       }
     }
   }
