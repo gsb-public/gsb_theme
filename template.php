e8008fa03ed29e89119ab5da74d03cd334645374
Tim Plunkett <git@plnktt.com>
2013-04-20 14:34:15 -0700

    Add theme function to remove the 'all day' label.

--- template.php
+++ template.php
@@ -1,1 +1,13 @@
 <?php
+
+/**
+ * @file
+ * Theme and preprocess functions for GSB Theme.
+ */
+
+/**
+ * Overrides theme_date_all_day_label().
+ */
+function gsb_theme_date_all_day_label($variables) {
+  return '';
+}
