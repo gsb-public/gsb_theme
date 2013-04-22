88c7e78e74d8f1771b8a505349db6534f3bb5aff
Tim Plunkett <git@plnktt.com>
2013-04-22 12:00:40 -0700

    Fix the date separator.

--- template.php
+++ template.php
@@ -11,3 +11,10 @@
 function gsb_theme_date_all_day_label($variables) {
   return '';
 }
+
+/**
+ * Overrides theme_date_display_range().
+ */
+function gsb_theme_date_display_range($variables) {
+  return $variables['date1'] . ' - ' . $variables['date2'];
+}
