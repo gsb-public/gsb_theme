ec00f40b9f673368d4811b2f7b15ee94c356a897
Tim Plunkett <git@plnktt.com>
2013-04-22 12:41:39 -0700

    Fix event listing CSS.

--- templates/views-view-unformatted--gsb-event.tpl.php
+++ templates/views-view-unformatted--gsb-event.tpl.php
@@ -5,10 +5,13 @@
  * Default simple view template to display a list of rows.
  *
  * @ingroup views_templates
+ *
+ * CHANGED:
+ *   h3 to h5
  */
 ?>
 <?php if (!empty($title)): ?>
-  <h3><?php print $title; ?></h3>
+  <h5><?php print $title; ?></h5>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
