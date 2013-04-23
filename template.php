ebd121b3700deaa42489eab972b5b008ad3ac828
Meghan Palagyi <meghanp@stanford.edu>
2013-04-23 11:33:52 -0700

    Clubs: WP-1019 form alter the search placeholder text.

--- template.php
+++ template.php
@@ -18,3 +18,13 @@ function gsb_theme_date_all_day_label($variables) {
 function gsb_theme_date_display_range($variables) {
   return $variables['date1'] . ' - ' . $variables['date2'];
 }
+
+/**
+ * Implements hook_form_FORM_ID_alter() for views_exposed_form().
+ */
+function gsb_theme_form_views_exposed_form_alter(&$form, &$form_state) {
+  if ($form['#id'] == 'views-exposed-form-clubs-search-search-club') {
+    // Add placeholder text.
+    $form['keyword']['#attributes']['placeholder'] = t('search within');
+  }
+}
