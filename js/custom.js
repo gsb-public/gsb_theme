79c010ecb7db10a44091f10e969f63bbe9ed5a10
Meghan Palagyi <meghanp@stanford.edu>
2013-04-24 15:20:00 -0700

    Bizin: change sidebar class for map js.

--- js/custom.js
+++ js/custom.js
@@ -163,7 +163,7 @@
   Drupal.behaviors.map_hover = {
     attach: function (context, settings) {
       if ($('.bi-map').length) {
-        var sidebar = $('.gsb-bizin-sidebar'),
+        var sidebar = $('.sidebar-inner'),
           biMenu = sidebar.find('.view-business-insights-sidebar'),
           biMap = sidebar.find('.bi-map'),
           biMaptext = biMap.find('.bi-map__text'),
