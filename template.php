92731c985c07df50eca90da9614c794aad738b98
Meghan Palagyi <meghanp@stanford.edu>
2013-04-24 23:13:47 -0700

    Search: use common classes.

--- template.php
+++ template.php
@@ -23,8 +23,29 @@ function gsb_theme_date_display_range($variables) {
  * Implements hook_form_FORM_ID_alter() for views_exposed_form().
  */
 function gsb_theme_form_views_exposed_form_alter(&$form, &$form_state) {
-  if ($form['#id'] == 'views-exposed-form-clubs-search-search-club') {
+  $search_form_ids = array(
+    'views-exposed-form-clubs-search-search-club',
+    'views-exposed-form-search-news-news-list',
+    'views-exposed-form-media-mention-media-mention-solr',
+    'views-exposed-form-publications-publications-solr',
+  );
+  $filter_form_ids = array(
+    'views-exposed-form-case-study-filters-case-study-list',
+    'views-exposed-form-club-filters-club-list',
+    'views-exposed-form-news-list-news-list',
+    'views-exposed-form-media-mention-filters-media-mention-list',
+    'views-exposed-form-publications-filters-publication-list'
+  );
+
+  if ($is_search_form = in_array($form['#id'], $search_form_ids)) {
     // Add placeholder text.
     $form['keyword']['#attributes']['placeholder'] = t('search within');
+    $form['#attributes']['class'][] = 'gsb-views-exposed-search';
+  }
+  if ($is_filter_form = in_array($form['#id'], $filter_form_ids)) {
+    $form['#attributes']['class'][] = 'gsb-views-exposed-filter';
+  }
+  if ($is_search_form || $is_filter_form) {
+    $form['#attributes']['class'][] = 'gsb-views-exposed-form';
   }
 }
