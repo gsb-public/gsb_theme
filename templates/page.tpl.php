0cadcd5a52b3c6c9f26dd178247c94d6db0e1996
Eugen <taracilaeugen@gmail.com>
2013-05-02 14:34:33 -0700

    WP-1159: Added 'Event Calendar'

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -86,8 +86,13 @@
       <?php if ($main_menu || $secondary_menu): ?>
         <div id="navigation"><div class="section">
           <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-          <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-        </div></div> <!-- /.section, /#navigation -->
+          <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+        </div><div class="se">
+			<a href="#" class="event-calendar">Event Calendar</a>
+			<?php
+				//Search input goes here
+			?>
+		</div></div><!-- /.section, /#navigation -->
       <?php endif; ?>
 
       <?php if ($site_name || $site_slogan): ?>
