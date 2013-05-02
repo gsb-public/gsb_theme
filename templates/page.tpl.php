7bf9f5098113442dbb00efe0b92ce8a82ce5b293
Meghan Palagyi <meghanp@stanford.edu>
2013-05-02 14:43:27 -0700

    Page: remove site name and secondary menu.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -83,10 +83,9 @@
         </a>
       <?php endif; ?>
 
-      <?php if ($main_menu || $secondary_menu): ?>
+      <?php if ($main_menu): ?>
         <div id="navigation"><div class="section">
           <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-          <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
         </div><div class="se">
 			<a href="#" class="event-calendar">Event Calendar</a>
 			<?php
@@ -95,24 +94,8 @@
 		</div></div><!-- /.section, /#navigation -->
       <?php endif; ?>
 
-      <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name"><strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong></div>
-            <?php else: /* Use h1 when the content title is empty */ ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </h1>
-            <?php endif; ?>
-          <?php endif; ?>
-
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div> <!-- /#name-and-slogan -->
+      <?php if ($site_slogan): ?>
+        <div id="site-slogan"><?php print $site_slogan; ?></div>
       <?php endif; ?>
 
       <?php print render($page['header']); ?>
