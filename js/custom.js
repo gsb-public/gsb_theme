2615b9bcdf3b35edaeff5a152885c7332fc515c3
32i <slava.merezhko@gmail.com>
2013-05-07 14:38:56 -0700

    wp-1056 added script for setting proper width for caption wrapper element in wysiwyg.

--- js/custom.js
+++ js/custom.js
@@ -201,4 +201,16 @@
       });
     }
   }
+
+  /**
+   * Check for caption wrapper around image and text,
+   * if it exist, set image width for wrapper element.
+   */
+  Drupal.behaviors.caption_wrapper_width = {
+    attach: function (context, settings) {
+      $('div.caption-center img, div.caption-left img, div.caption-right img').each(function() {
+        $(this).parent().css('width',$(this).attr('width'));
+      });
+    }
+  }
 }(jQuery));
