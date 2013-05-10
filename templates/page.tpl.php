ae554fe6045b9a83579340fa4e2995d016622c37
Meghan Palagyi <meghanp@stanford.edu>
2013-05-10 11:55:00 -0700

    Responsive: WP-1311 main menu at 768.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -77,21 +77,14 @@
 
     <div id="header-wrapper"><div id="header"><div class="section clearfix">
 
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
-
       <?php if ($main_menu): ?>
         <div id="navigation"><div class="section">
           <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        </div><div class="se">
-      <a href="/events" class="event-calendar">Event Calendar</a>
-      <?php
-        //Search input goes here
-      ?>
-    </div></div><!-- /.section, /#navigation -->
+          <div class="se">
+            <a href="/events" class="event-calendar">Event Calendar</a>
+            <!-- Search input goes here -->
+          </div><!-- /.se -->
+        </div></div><!-- /.section, /#navigation -->
       <?php endif; ?>
 
       <?php if ($site_slogan): ?>
@@ -99,6 +92,12 @@
       <?php endif; ?>
 
       <?php print render($page['header']); ?>
+
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
 
     </div></div></div> <!-- /.section, /#header, /#header-wrapper -->
 
