973b71204d4d365ae9631946f5bd3ba965a336cb
Alexei Gorobets <asgorobets@gmail.com>
2013-06-07 14:53:28 +0300

    Add breadcrumbs to main region in templete.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -138,6 +138,9 @@
         <div id="main" class="column"><div class="section">
           <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
           <a id="main-content"></a>
+          <?php if (!isset($page['banner']['gsb_public_custom_blocks_gpcb_breadcrumbs'])): ?>
+          <?php print $breadcrumb; ?>
+          <?php endif; ?>
           <?php print render($title_prefix); ?>
           <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
           <?php print render($title_suffix); ?>
