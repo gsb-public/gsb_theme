8775cc25205c9b3679aec7567400c03ad9ec6d42
Greg Mercer <gmercer@stanford.edu>
2013-06-11 15:27:40 -0600

    Part of WP-1575 Added handling for links on the first page of add_content_modal.

--- templates/panels-add-content-link.tpl.php
+++ templates/panels-add-content-link.tpl.php
@@ -4,12 +4,12 @@
  * Template to control the add content individual links in the add content modal.
  */
 ?>
-<?php if (isset($content_type['top level'])): ?>
+<?php if (isset($content_type['top level']) || $first_page == TRUE ): ?>
 <li>
   <?php print $text_button; ?>
 </li>
 <?php endif; ?>
-<?php if (!isset($content_type['top level'])): ?>
+<?php if (!isset($content_type['top level']) && $first_page != TRUE): ?>
 <div class="content-type-button clearfix">
   <?php print $image_button; ?>
   <div><?php print $text_button; ?></div>
