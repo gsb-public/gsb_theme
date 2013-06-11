a4c503fb8969e83c9952faa3768311fbbc608860
Greg Mercer <gmercer@stanford.edu>
2013-06-10 19:21:03 -0600

    Added check if top level menu is being displayed vs the content Add button.

--- templates/panels-add-content-link.tpl.php
+++ templates/panels-add-content-link.tpl.php
@@ -4,6 +4,14 @@
  * Template to control the add content individual links in the add content modal.
  */
 ?>
+<?php if (isset($content_type['top level'])): ?>
 <li>
   <?php print $text_button; ?>
 </li>
+<?php endif; ?>
+<?php if (!isset($content_type['top level'])): ?>
+<div class="content-type-button clearfix">
+  <?php print $image_button; ?>
+  <div><?php print $text_button; ?></div>
+</div>
+<?php endif; ?>
