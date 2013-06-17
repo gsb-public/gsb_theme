e49593feb437483903b6a3980aec93607af82b05
Meghan Palagyi <meghanp@stanford.edu>
2013-06-17 14:31:27 -0700

    WP-1604: mobile events filter fixes.

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -51,7 +51,7 @@ Drupal.behaviors.responsiveEventsfilter = {
 
 Drupal.behaviors.responsiveEventsfacet = {
   attach: function (context, settings) {
-    $(context).find('#views-exposed-form-gsb-event-panel-pane-2 .views-exposed-widget label').click(function() {
+    $(context).find('#views-exposed-form-gsb-event-event-listing-pane .views-exposed-widget label').click(function() {
       $(this).parent().toggleClass('open');
     });
   }
