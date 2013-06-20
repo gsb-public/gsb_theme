e53a55453d5816fa545966be6d66fd454d894fba
Greg Mercer <gmercer@stanford.edu>
2013-06-20 12:30:28 -0600

    Added checking for stock content page handling.

--- templates/panels-add-content-link.tpl.php
+++ templates/panels-add-content-link.tpl.php
@@ -4,12 +4,12 @@
  * Template to control the add content individual links in the add content modal.
  */
 ?>
-<?php if (isset($content_type['top level']) || $first_page == TRUE ): ?>
+<?php if (isset($content_type['top level']) || $first_page == TRUE || $stock_content_page == TRUE ): ?>
 <li>
   <?php print $text_button; ?>
 </li>
 <?php endif; ?>
-<?php if (!isset($content_type['top level']) && $first_page != TRUE): ?>
+<?php if (!isset($content_type['top level']) && $first_page != TRUE && $stock_content_page != TRUE): ?>
 <div class="content-type-button clearfix">
   <?php print $image_button; ?>
   <div><?php print $text_button; ?></div>
