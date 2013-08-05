e42102eb4e1694cbac2479af38fed5f49b9f50ae
Meghan Palagyi <meghanp@stanford.edu>
2013-08-05 10:34:30 -0700

    WP-1970: condense menus js.

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -3,57 +3,13 @@
 
 Drupal.behaviors.responsiveMenu = {
   attach: function (context, settings) {
-    $(context).find('#nav-touch-button').click(function() {
-      $(this).parent().toggleClass('open');
-    });
-  }
-};
-
-Drupal.behaviors.responsivePrograms = {
-  attach: function (context, settings) {
-    $(context).find('#program-links-title').click(function() {
-      $(this).parent().toggleClass('open');
-    });
-  }
-};
-
-Drupal.behaviors.responsiveSearch = {
-  attach: function (context, settings) {
-    $(context).find('#search-touch-button').click(function() {
-      $('#google-appliance-block-form').toggleClass('open');
-    });
-  }
-};
-
-Drupal.behaviors.responsiveMenutoggle = {
-  attach: function (context, settings) {
-    $(context).find('#block-system-main-menu li').click(function() {
-      $(this).toggleClass('open');
-    });
-  }
-};
-
-Drupal.behaviors.responsiveSectionmenu = {
-  attach: function (context, settings) {
-    $(context).find('#sidebar h2').click(function() {
-      $(this).parent().toggleClass('open');
-    });
-  }
-};
-
-Drupal.behaviors.responsiveEventsfilter = {
-  attach: function (context, settings) {
-    $(context).find('.pane-block h2.pane-title').click(function() {
-      $(this).parent().toggleClass('open');
-    });
-  }
-};
-
-Drupal.behaviors.responsiveEventsfacet = {
-  attach: function (context, settings) {
-    $(context).find('#views-exposed-form-gsb-event-event-listing-pane .views-exposed-widget label').click(function() {
-      $(this).parent().toggleClass('open');
-    });
+    $(context).find('#nav-touch-button').click(function() { $(this).parent().toggleClass('open');});
+    $(context).find('#program-links-title').click(function() { $(this).parent().toggleClass('open');});
+    $(context).find('#sidebar h2').click(function() { $(this).parent().toggleClass('open');});
+    $(context).find('.pane-block h2.pane-title').click(function() { $(this).parent().toggleClass('open');});
+    $(context).find('#views-exposed-form-gsb-event-event-listing-pane .views-exposed-widget label').click(function() { $(this).parent().toggleClass('open');});
+    $(context).find('#search-touch-button').click(function() { $('#google-appliance-block-form').toggleClass('open');});
+    $(context).find('#block-system-main-menu li').click(function() { $(this).toggleClass('open');});
   }
 };
 
