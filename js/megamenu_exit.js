b30a93686ec39c381b3525d05febdd7c51e94c26
Adam Moore <redndahead@gmail.com>
2013-08-06 10:08:50 -0700

    Add support for touch devices so if the width is larger than 568px it needs to be clicked twice. Once to open the menu and the other to go to the link.

--- js/megamenu_exit.js
+++ js/megamenu_exit.js
@@ -10,6 +10,27 @@ Drupal.behaviors.megamenuexit = {
       return;
     }
 
+    // If the width is greater than 568px and it's a touch device make the user
+    // double click.
+    if ($('html').hasClass('touch') && Modernizr.mq('(min-width: 569px)')) {
+      // Trap the click event on the top megamenu items.
+      $('a.menu-minipanel').click(function() {
+        // If it's been clicked once then go to the site.
+        if ($(this).hasClass('clickable')) {
+          return true;
+        }
+        // If it hasn't been clicked add a class to show it's been clicked once
+        else {
+          // Remove all the clickable classes so we can move between menus.
+          $('a.menu-minipanel').removeClass('clickable');
+
+          // Add the clickable class and stop any action from happening.
+          $(this).addClass('clickable');
+          return false;
+        }
+      });
+    }
+
     // Loop through each menu-minipanel and add a close button on hover.
     for (var i in settings.menuMinipanels.panels) {
       if (settings.menuMinipanels.panels.hasOwnProperty(i)) {
@@ -21,7 +42,12 @@ Drupal.behaviors.megamenuexit = {
           });
       }
     }
+
     $(document).on('click', 'a.qtip-close', function() {
+      // Remove all clickable classes so we can click again.
+      $('a.menu-minipanel').removeClass('clickable');
+
+      // Hide the qtip
       $('.qtip-megamenu').slideUp();
     });
   }
