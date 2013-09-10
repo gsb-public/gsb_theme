c578f11f00cfc612821416028d20570441260706
Meghan Palagyi <meghanp@stanford.edu>
2013-09-10 09:18:01 -0700

    WPBQA-402: adjust js sheet.

--- js/libs/responsive-tables.js
+++ js/libs/responsive-tables.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+(function ($) {
   var switched = false;
   var updateTables = function() {
     if (($(window).width() < 767) && !switched ){
@@ -15,27 +15,27 @@ $(document).ready(function() {
       });
     }
   };
-   
+
   $(window).load(updateTables);
   $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
   $(window).on("resize", updateTables);
-   
-	
+
+
 	function splitTable(original)
 	{
 		original.wrap("<div class='table-wrapper' />");
-		
+
 		var copy = original.clone();
 		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
 		copy.removeClass("responsive");
-		
+
 		original.closest(".table-wrapper").append(copy);
 		copy.wrap("<div class='pinned' />");
 		original.wrap("<div class='scrollable' />");
 
     setCellHeights(original, copy);
 	}
-	
+
 	function unsplitTable(original) {
     original.closest(".table-wrapper").find(".pinned").remove();
     original.unwrap();
@@ -64,4 +64,4 @@ $(document).ready(function() {
     });
   }
 
-});
+}(jQuery));
