fe7626c0affaba3bf4ea674f88626bc212b7facd
Meghan Palagyi <meghanp@stanford.edu>
2013-10-07 14:28:51 -0700

    WP-2062: implement styles.

--- templates/views-view-unformatted--phd-candidate-placement-listing--panel-pane-phd-placement-listing.tpl.php
+++ templates/views-view-unformatted--phd-candidate-placement-listing--panel-pane-phd-placement-listing.tpl.php
@@ -5,13 +5,30 @@
  * Default simple view template to display a list of rows.
  *
  * @ingroup views_templates
+ *
+ * CHANGED
+ *
+ * H3 to H4
+ * Added phd-placement-wrapper around title and row.
+ * Added Back to top pager link after each grouping.
  */
 ?>
-<?php if (!empty($title)): ?>
-  <h3><?php print $title; ?></h3>
-<?php endif; ?>
-<?php foreach ($rows as $id => $row): ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-    <?php print $row; ?>
+<div class="phd-placement-wrapper">
+  <?php if (!empty($title)): ?>
+    <h4 class="views-field-name"><?php print $title; ?></h4>
+  <?php endif; ?>
+  <div class="phd-row-wrapper">
+    <?php foreach ($rows as $id => $row): ?>
+      <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+        <?php print $row; ?>
+      </div>
+    <?php endforeach; ?>
   </div>
-<?php endforeach; ?>
+</div>
+<div class="pager-region">
+  <div class="item-list">
+    <ul class="additional-links">
+      <li class="go-to-top-link first last"><a href="#placement-listing-top">Back to the Top</a></li>
+    </ul>
+  </div>
+</div>
