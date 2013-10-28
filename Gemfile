eb0f289b68bca5152a3b4944fd6f65a261c677fb
Meghan Palagyi <meghanp@stanford.edu>
2013-10-28 15:59:31 -0700

    WP-2284: include preexisting requirements.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 # Gemfile
-source "https://rubygems.org"
-gem "compass", ">=0.13.alpha.0"
-gem "breakpoint"
-gem "singularitygs", ">=0.0.15.1"
+source 'https://rubygems.org'
+ruby '1.8.7'
+gem 'compass', '>=0.13.alpha.0'
+gem 'breakpoint'
+gem 'singularitygs', '>=0.0.15.1'
--- config.rb
+++ config.rb
@@ -1,38 +1,31 @@
+# Require Rubygems and Bundler
+require 'rubygems'
+require 'bundler/setup'
+
+# Require Sass and Compass
 require 'sass'
 require 'compass'
 
-# Require any additional compass plugins here.
+# Require additional gems
 require 'breakpoint'
 require 'singularitygs'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-fonts_dir = "fonts"
-images_dir = "images"
-javascripts_dir = "js"
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+fonts_dir = 'fonts'
+images_dir = 'images'
+javascripts_dir = 'js'
 
-# Also assuming this theme is in sites/*/themes/THEMENAME, you can add the partials
-# included with a module by uncommenting and modifying one of the lines below:
-#add_import_path "../../../default/modules/FOO"
-#add_import_path "../../../all/modules/FOO"
-#add_import_path "../../../../modules/FOO"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
+# Enable relative paths to assets
 relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
+# Disable debugging comments
 line_comments = false
 
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
+# Indented syntax
 preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
 # Add cache buster
 asset_cache_buster :none
