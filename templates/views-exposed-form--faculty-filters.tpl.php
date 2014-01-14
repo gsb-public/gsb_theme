ebd8943eb4feb47429e69228b4e6efac40d5886a
Meghan Palagyi <meghanp@stanford.edu>
2014-01-14 15:26:55 -0800

    WP-2611: Customize exposed form template for faculty filters.

--- templates/views-exposed-form--faculty-filters.tpl.php
+++ templates/views-exposed-form--faculty-filters.tpl.php
@@ -27,6 +27,31 @@
   ?>
 <?php endif; ?>
 <div class="views-exposed-form">
+  <div class="search-wrap-faculty clearfix">
+    <div id="<?php print $search_widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
+      <?php if (!empty($search_widget->label)): ?>
+        <label for="<?php print $search_widget->id; ?>">
+          <?php print $search_widget->label; ?>
+        </label>
+      <?php endif; ?>
+      <?php if (!empty($search_widget->operator)): ?>
+        <div class="views-operator">
+          <?php print $search_widget->operator; ?>
+        </div>
+      <?php endif; ?>
+      <div class="views-widget">
+        <?php print $search_widget->widget; ?>
+      </div>
+      <?php if (!empty($search_widget->description)): ?>
+        <div class="description">
+          <?php print $search_widget->description; ?>
+        </div>
+      <?php endif; ?>
+    </div>
+    <div class="views-exposed-widget views-submit-button">
+      <?php print $button; ?>
+    </div>
+  </div>
   <div class="views-exposed-widgets clearfix">
     <?php foreach ($widgets as $id => $widget): ?>
       <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
@@ -68,13 +93,32 @@
         <?php print $offset; ?>
       </div>
     <?php endif; ?>
-    <div class="views-exposed-widget views-submit-button">
-      <?php print $button; ?>
-    </div>
     <?php if (!empty($reset_button)): ?>
       <div class="views-exposed-widget views-reset-button">
         <?php print $reset_button; ?>
       </div>
     <?php endif; ?>
   </div>
+  <?php foreach ($az_widgets as $id => $widget): ?>
+    <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
+      <?php if (!empty($widget->label)): ?>
+        <label for="<?php print $widget->id; ?>">
+          <?php print $widget->label; ?>
+        </label>
+      <?php endif; ?>
+      <?php if (!empty($widget->operator)): ?>
+        <div class="views-operator">
+          <?php print $widget->operator; ?>
+        </div>
+      <?php endif; ?>
+      <div class="views-widget">
+        <?php print $widget->widget; ?>
+      </div>
+      <?php if (!empty($widget->description)): ?>
+        <div class="description">
+          <?php print $widget->description; ?>
+        </div>
+      <?php endif; ?>
+    </div>
+  <?php endforeach; ?>
 </div>
