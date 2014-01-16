e6fb9a85e0cb16ac5519ebe6e270d5e199b74452
Meghan Palagyi <meghanp@stanford.edu>
2014-01-16 15:42:42 -0800

    WP-2655: add default template.

---
+++ templates/ds-2col--node-case.tpl.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 2 column template.
+ *
+ * CHANGES:
+ *  - Removed clearfix
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-2col <?php print $classes;?>">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <<?php print $left_wrapper ?> class="group-left<?php print $left_classes; ?>">
+    <?php print $left; ?>
+  </<?php print $left_wrapper ?>>
+
+  <<?php print $right_wrapper ?> class="group-right<?php print $right_classes; ?>">
+    <?php print $right; ?>
+  </<?php print $right_wrapper ?>>
+
+</<?php print $layout_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
