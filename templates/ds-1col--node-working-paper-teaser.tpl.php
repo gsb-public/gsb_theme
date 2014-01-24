143818659334702f2175672d8551951da2afcd19
Meghan Palagyi <meghanp@stanford.edu>
2014-01-24 15:54:01 -0800

    WP-2712: add template for working paper teaser.

---
+++ templates/ds-1col--node-working-paper-teaser.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 1 column template.
+ *
+ * CHANGES:
+ *  - Removed clearfix
+ */
+?>
+<<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?>">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php print $ds_content; ?>
+</<?php print $ds_content_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
