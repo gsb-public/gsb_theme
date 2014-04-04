541005efa7a254758430ba89b18ed1f068a36e9b
Tim Plunkett <git@plnktt.com>
2014-04-04 14:08:43 -0700

    WP-3271: Move tweetfeed code to social_feed module.

--- js/custom.js
+++ js/custom.js
@@ -148,32 +148,6 @@
   };
 
   /**
-   * Check for social field fpp, if it exists, check the source.
-   * If the source is twitter, grab the values from fields and
-   * transmit them to gsb_tweetfeed function.
-   */
-  Drupal.behaviors.social_feed = {
-    attach: function () {
-      var socialWrapper = $('.pane-bundle-social-feed');
-      if (socialWrapper.length > 0 && !socialWrapper.hasClass('processed')) {
-        var _source = socialWrapper.find('.field-name-field-feed-source').text();
-        if ( _source.toLowerCase() == 'twitter') {
-          // Get tweets number and search query.
-          var _length = socialWrapper.find('.field-name-field-social-display-num').text(),
-            _search = socialWrapper.find('.field-name-field-twitter-search').text();
-
-          // Initialize twitterfeed.js function.
-          gsb_tweetfeed.init({
-            search: _search,
-            numTweets: _length,
-            appendTo: '.pane-bundle-social-feed'
-          });
-        }
-      }
-    }
-  };
-
-  /**
    * Move quicklinks to top of page in mobile landscape context.
    * Move search to top of page in mobile landscape context.
    */
