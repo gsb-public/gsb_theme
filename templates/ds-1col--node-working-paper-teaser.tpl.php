1332c8dec974655ea9a6d7ea62c4a8af253ec997
Meghan Palagyi <meghanp@stanford.edu>
2014-05-06 11:03:07 -0700

    WP-3552: update styling for list view.

--- templates/ds-1col--node-working-paper-teaser.tpl.php
+++ templates/ds-1col--node-working-paper-teaser.tpl.php
@@ -14,9 +14,18 @@
 
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
-  <?php endif; ?>
-  <div class="featured-label">Featured Working Paper</div>
-  <?php print $ds_content; ?>
+<?php endif; ?>
+
+<?php if ($use_photo) print render($photo); ?>
+<div class="person-fpp-wrapper">
+<?php print render($name); ?>
+<?php if ($use_rank) print '<span class="field field-name-field-rank">' . render($rank) . '</span>'; ?>
+<?php if ($use_academic_area) print '<span class="field field-name-field-academic-area">' . render($academic_area) . '</span>'; ?>
+<?php print render($program); ?>
+<?php if ($use_phone) print '<div class="field field-name-field-phone">' . render($phone) . '</div>'; ?>
+<?php if ($use_email) print '<div class="field field-name-field-email"><a href="mailto:' . render($email) . '">Email</a></div>'; ?>
+<?php print render($summary); ?>
+</div>
 <?php print '</' . $ds_content_wrapper . '>' ?>
 
 <?php if (!empty($drupal_render_children)): ?>
