76b6312f3ca03bcfff0cd73e77c036ab7c6c5783
Meghan Palagyi <meghanp@stanford.edu>
2014-08-19 10:45:45 -0700

    WP-3993: add figure wrapper.

--- templates/ds-1col--file-resource-image.tpl.php
+++ templates/ds-1col--file-resource-image.tpl.php
@@ -7,13 +7,16 @@
  * CHANGES:
  *  - Removed clearfix
  *  - Remove line break white space
+ *  - Add figure wrapper
  */
 ?>
 <<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?>">
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
-  <?php print $ds_content; ?>
+  <figure>
+    <?php print $ds_content; ?>
+  </figure>
 <?php print '</' . $ds_content_wrapper . '>' ?>
 <?php if (!empty($drupal_render_children)): ?>
   <?php print $drupal_render_children ?>
