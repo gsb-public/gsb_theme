150e48d3d437b82974a38abd604b999250d80c7f
Meghan Palagyi <meghanp@stanford.edu>
2014-12-03 11:43:03 -0800

    WP-4738: override file field to open in a new tab.

--- template.php
+++ template.php
@@ -414,3 +414,33 @@ function gsb_theme_footnote_list($footnotes) {
   $str .= "</ul>\n";
   return $str;
 }
+/**
+ * Override the file link field so that files will open in a new tab.
+ */
+function gsb_theme_file_link($variables) {
+  $file = $variables['file'];
+  $icon_directory = $variables['icon_directory'];
+
+  $url = file_create_url($file->uri);
+  $icon = theme('file_icon', array('file' => $file, 'icon_directory' => $icon_directory));
+
+  // Set options as per anchor format described at
+  // http://microformats.org/wiki/file-format-examples
+  $options = array(
+    'attributes' => array(
+      'type' => $file->filemime . '; length=' . $file->filesize,
+      'target' => '_blank',
+    ),
+  );
+
+  // Use the description as the link text if available.
+  if (empty($file->description)) {
+    $link_text = $file->filename;
+  }
+  else {
+    $link_text = $file->description;
+    $options['attributes']['title'] = check_plain($file->filename);
+  }
+
+  return '<span class="file">' . $icon . ' ' . l($link_text, $url, $options) . '</span>';
+}
