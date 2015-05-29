00024db095dc8bcc40eb4d9b9db3e084df38cc09
Meghan Palagyi <meghanp@stanford.edu>
2015-05-29 14:51:20 -0400

    WP-5034: changes for working paper pdf.

---
+++ templates/ds-1col--node-working-paper-teaser.tpl.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 1 column template.
+ *
+ * CHANGES:
+ *  - Removed clearfix
+ *  - Add featured label
+ *  - Remove line break white space
+ */
+?>
+<<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?>">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+  <div class="featured-label">Featured Working Paper</div>
+  <?php print $ds_content; ?>
+<?php print '</' . $ds_content_wrapper . '>' ?>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
