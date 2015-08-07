810f7e6f35c7f76dfe33a751a3724da79b6595dd
Adam Moore <redndahead@gmail.com>
2015-08-07 13:35:50 -0700

    WP-5612: Make it work more globally. Focus the search field when first clicked.

--- js/search.js
+++ js/search.js
@@ -6,10 +6,11 @@
   Drupal.behaviors.gsb_search_box = {
     attach: function () {
       $('#navigation').addClass('search-closed');
-      $('#navigation .global-search #edit-submit').click(function(e) {
+      $('.global-search input[type="submit"]').click(function(e) {
         if ($('#navigation').hasClass('search-closed')) {
           $('#navigation').removeClass('search-closed');
           $('#navigation').addClass('search-open');
+          $('.global-search input[name="search_keys"]').focus();
           e.preventDefault();
         }
       });
