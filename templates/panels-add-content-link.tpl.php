ba4a500be8cc005ead4bf08396978c301ebf439b
Greg Mercer <gmercer@stanford.edu>
2015-11-06 08:13:15 -0800

    WP-6152 Changed to use empty() check for $first_page and $stock_content_page variables

--- templates/panels-add-content-link.tpl.php
+++ templates/panels-add-content-link.tpl.php
@@ -4,14 +4,15 @@
  * Template to control the add content individual links in the add content modal.
  */
 ?>
-<?php if (isset($content_type['top level']) || $first_page == TRUE || $stock_content_page == TRUE ): ?>
+<?php if (isset($content_type['top level']) || !empty($first_page) || !empty($stock_content_page) ): ?>
 <li>
   <?php print $text_button; ?>
 </li>
 <?php endif; ?>
-<?php if (!isset($content_type['top level']) && $first_page != TRUE && $stock_content_page != TRUE): ?>
+<?php if (!isset($content_type['top level']) && empty($first_page) && empty($stock_content_page) ): ?>
 <div class="content-type-button clearfix">
   <?php print $image_button; ?>
   <div><?php print $text_button; ?></div>
 </div>
 <?php endif; ?>
+
