4bb841d4fe65660f58c4a68dda3ee2d4f10c1c1d
Meghan Plunkett <meghanp@stanford.edu>
2016-01-14 15:59:45 -0500

    WP-5064: use flexbox for insights layouts.

--- layouts/gsb-insights-a/gsb-insights-a.tpl.php
+++ layouts/gsb-insights-a/gsb-insights-a.tpl.php
@@ -21,88 +21,84 @@
 
     <div class="insights-main-wrapper">
 
-      <?php if ($content['contenttwoone'] || $content['contenttwotwo'] || $content['contenttwothree'] || $content['contenttwofour'] || $content['contenttwofive']): ?>
-        <div class="wide-narrow-narrow">
-          <div class="column first">
-            <div class="wide-view alpha">
-              <?php print $content['contenttwoone']; ?>
-            </div>
-            <div class="narrow-view beta">
-              <?php print $content['contenttwotwo']; ?>
-            </div>
-            <div class="narrow-view gamma">
-              <?php print $content['contenttwothree']; ?>
-            </div>
-          </div>
-          <div class="column second narrow-view">
-            <?php print $content['contenttwofour']; ?>
-          </div>
-          <div class="column third narrow-view">
-            <?php print $content['contenttwofive']; ?>
-          </div>
+      <?php if ($content['contenttwoone']): ?>
+        <div class="wide-view">
+          <?php print $content['contenttwoone']; ?>
         </div>
       <?php endif; ?>
 
-      <?php if ($content['contentthreeone'] || $content['contentthreetwo'] || $content['contentthreethree'] || $content['contentthreefour'] || $content['contentthreefive']): ?>
-        <div class="narrow-narrow-wide">
-          <div class="column first narrow-view">
-            <?php print $content['contentthreeone']; ?>
-          </div>
-          <div class="column second narrow-view">
-            <?php print $content['contentthreetwo']; ?>
-          </div>
-          <div class="column third">
-            <div class="wide-view alpha">
-              <?php print $content['contentthreethree']; ?>
-            </div>
-            <div class="narrow-view beta">
-              <?php print $content['contentthreefour']; ?>
-            </div>
-            <div class="narrow-view gamma">
-              <?php print $content['contentthreefive']; ?>
-            </div>
-          </div>
+      <?php if ($content['contenttwotwo']): ?>
+        <div class="narrow-view">
+          <?php print $content['contenttwotwo']; ?>
         </div>
       <?php endif; ?>
 
-      <?php if ($content['contentfourone'] || $content['contentfourtwo'] || $content['contentfourthree'] || $content['contentfourfour'] || $content['contentfourfive']): ?>
-        <div class="narrow-wide-narrow">
-          <div class="column first narrow-view">
-            <?php print $content['contentfourone']; ?>
-          </div>
-          <div class="column second">
-            <div class="wide-view alpha">
-              <?php print $content['contentfourtwo']; ?>
-            </div>
-            <div class="narrow-view beta">
-              <?php print $content['contentfourthree']; ?>
-            </div>
-            <div class="narrow-view gamma">
-              <?php print $content['contentfourfour']; ?>
-            </div>
-          </div>
-          <div class="column third narrow-view">
-            <?php print $content['contentfourfive']; ?>
-          </div>
+      <?php if ($content['contenttwothree']): ?>
+        <div class="narrow-view">
+          <?php print $content['contenttwothree']; ?>
         </div>
       <?php endif; ?>
 
-      <?php if ($content['contentfiveone'] || $content['contentfivetwo'] || $content['contentfivethree'] || $content['contentfivefour']): ?>
-        <div class="narrow-narrow-narrow-narrow">
-          <div class="column first narrow-view">
-            <?php print $content['contentfiveone']; ?>
-          </div>
-          <div class="column second narrow-view">
-            <?php print $content['contentfivetwo']; ?>
-          </div>
-          <div class="column third narrow-view">
-            <?php print $content['contentfivethree']; ?>
-          </div>
-          <div class="column fourth narrow-view">
-            <?php print $content['contentfivefour']; ?>
-          </div>
+      <?php if ($content['contentthreeone']): ?>
+        <div class="narrow-view">
+          <?php print $content['contentthreeone']; ?>
         </div>
       <?php endif; ?>
+
+      <?php if ($content['contentthreetwo']): ?>
+        <div class="narrow-view">
+          <?php print $content['contentthreetwo']; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($content['contentthreethree']): ?>
+        <div class="wide-view">
+          <?php print $content['contentthreethree']; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($content['contentfourone']): ?>
+        <div class="narrow-view">
+          <?php print $content['contentfourone']; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($content['contentfourtwo']): ?>
+        <div class="wide-view">
+          <?php print $content['contentfourtwo']; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($content['contentfourthree']): ?>
+        <div class="narrow-view">
+          <?php print $content['contentfourthree']; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($content['contentfiveone']): ?>
+        <div class="narrow-view">
+          <?php print $content['contentfiveone']; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($content['contentfivetwo']): ?>
+        <div class="narrow-view">
+          <?php print $content['contentfivetwo']; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($content['contentfivethree']): ?>
+        <div class="narrow-view">
+          <?php print $content['contentfivethree']; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($content['contentfivefour']): ?>
+        <div class="narrow-view">
+          <?php print $content['contentfivefour']; ?>
+        </div>
+      <?php endif; ?>
+
     </div><!-- /.insights-main-wrapper -->
   </div><!-- /.insights-card-wrapper -->
 
