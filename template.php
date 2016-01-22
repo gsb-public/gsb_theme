de737ba2b09450897202b73a4b270800f59d7425
Greg Mercer <gmercer@stanford.edu>
2016-01-22 13:32:54 -0800

    Added check for menu-executive-education-mega-me when preprocessing menu_link

--- template.php
+++ template.php
@@ -363,7 +363,8 @@ function gsb_theme_preprocess_field(&$variables, $hook) {
 function gsb_theme_preprocess_menu_link(&$variables) {
   // Need to remove any menu minipanels code on anything that isn't the main
   // menu.
-  if ($variables['element']['#original_link']['menu_name'] !== 'main-menu') {
+  if ($variables['element']['#original_link']['menu_name'] !== 'main-menu' &&
+    $variables['element']['#original_link']['menu_name']!== 'menu-executive-education-mega-me') {
     unset($variables['element']['#localized_options']['menu_minipanels_hover']);
   }
 }
