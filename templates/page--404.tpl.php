8fe24e02cc48c901d293f21ffac6088d4c96fcf3
Ayuta Padhi <apadhi@stanford.edu>
2016-11-07 15:37:01 -0800

    WP-7697 Updated the html

--- templates/page--404.tpl.php
+++ templates/page--404.tpl.php
@@ -29,33 +29,42 @@
         <div id="main">
           <div class="error-wrapper">
             <h1>Sorry! The page you are looking for could not be found</h1>
-            <h2>Please try our <a href="/search" title="Search">site search</a>, or explore the links below.</h2>
-            <div class="menu-wrapper">
-              <h3>Site Links</h3>
+            <h2>Try our <a href="/search" title="Search">site search</a> or explore our site using the links below.</h2>
+            <div class="menu-wrapper" width="33%">
+              <h3>Programs & Research</h3>
               <ul class="menu">
-                <li><a href="/" title="Home">Home</a></li>
-                <li><a href="/stanford-gsb-experience" title="The Experience">The Experience</a></li>
                 <li><a href="/programs" title="Our Programs">The Programs</a></li>
+                <li><a href="/exec-ed" title="Executive Education">Executive Education</a></li>
+                <li><a href="/programs/stanford-ignite" title="Stanford Ignite">Stanford Ignite</a></li>
                 <li><a href="/faculty-research" title="Faculty and Research">Faculty &amp; Research</a></li>
-                <li><a href="/insights" title="Insights by Stanford Business">Insights by Stanford Business</a></li>
-                <li><a href="/alumni" title="Alumni">Alumni</a></li>
-                <li><a href="/organizations" title="Companies, Organizations and Recruiters">Companies, Organizations &amp; Recruiters</a></li>
-                <li><a href="/stanford-university-community" title="Stanford University Community">Stanford University Community</a></li>
-                <li><a href="/library" title="library">Library</a></li>
+                <li><a href="/seed" title="Stanford Seed">Stanford Seed</a></li>
+                <li><a href="/insights" title="Insights by Stanford Business">Insights by <em>Stanford Business</em></a></li>
               </ul>
             </div>
-
-            <div class="menu-wrapper">
-              <h3>About Us</h3>
+            <div class="menu-wrapper" width="33%">
+              <h3>About Stanford GSB</h3>
               <ul class="menu">
-                <li><a href="/events" title="Events">Events</a></li>
-                <li><a href="/newsroom" title="Newsroom">Newsroom</a></li>
+                <li><a href="/stanford-gsb-experience/news-history" title="School News & History">School News & History</a></li>
+                <li><a href="/stanford-gsb-experience" title="The Experience">The Experience</a></li>
                 <li><a href="/visit" title="Visit Us">Visit Us</a></li>
                 <li><a href="/contact" title="Contact Us">Contact Us</a></li>
                 <li><a href="/jobs" title="Jobs">Jobs</a></li>
-                <li><a href="http://mygsb.stanford.edu" title="MyGSB">MyGSB</a></li>
               </ul>
             </div>
+            <div class="menu-wrapper" width="34%">
+              <h3>Connect With Stanford GSB</h3>
+              <ul class="menu">
+                <li><a href="/events" title="Events">Events</a></li>
+                <li><a href="/alumni" title="Alumni">Alumni</a></li>
+                <li><a href="/alumni/giving" title="Giving">Giving</a></li>
+                <li><a href="/organizations" title="Companies, Organizations and Recruiters">Companies, Organizations &amp; Recruiters</a></li>
+                <li><a href="/stanford-university-community" title="Stanford University Community">Stanford University Community</a></li>
+                <li><a href="/newsroom" title="Newsroom">Newsroom</a></li>
+                <li><a href="/library" title="Business Library">Business Library</a></li>
+              </ul>
+            </div>
+            <p><a href="http://mygsb.stanford.edu" title="MyGSB" target="_blank">Log In to MyGSB</a></p>
+
 
           </div></div> <!-- /.error-wrapper, /#main -->
 
