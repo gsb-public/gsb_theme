29b60ca515c0b2fb432941048adb64459ec68ef0
Ayuta Padhi <apadhi@stanford.edu>
2017-05-22 15:00:27 -0700

    WP-7829 Added Google Optimize code to html.tpl.php

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -59,6 +59,26 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
+  {quote}<HTML>
+  <HEAD>
+    <!-- Page hiding snippet (recommended)  -->
+    <style>.async-hide { opacity: 0 !important} </style>
+    <script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+        h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+        (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+      })(window,document.documentElement,'async-hide','dataLayer',4000,
+        {'GTM-XXXXXX':true});</script>
+    <!-- Modified Analytics tracking code with Optimize plugin -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-17436788-1', 'auto');  // Update tracker settings
+      ga('require', 'GTM-57CTFLR');           // Add this line
+      // Remove pageview call
+    </script>{quote}
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <!--googleoff: index--><!--googleoff: snippet-->
