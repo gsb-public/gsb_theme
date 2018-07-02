dfdb54ea53fecc835744c9eb51fbd40392bfe526
Greg Mercer <gmercer@stanford.edu>
2018-07-02 06:53:00 -0700

    Changing search block form to be coveo-search-block-form

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -8,7 +8,7 @@ Drupal.behaviors.responsiveMenu = {
     $(context).find('#sidebar h2').click(function() { $(this).parent().toggleClass('open');});
     $(context).find('.pane-block h2.pane-title').click(function() { $(this).parent().toggleClass('open');});
     $(context).find('#views-exposed-form-gsb-event-event-listing-pane .views-exposed-widget label').click(function() { $(this).parent().toggleClass('open');});
-    $(context).find('#search-touch-button').click(function() { $('#google-appliance-block-form').toggleClass('open');});
+    $(context).find('#search-touch-button').click(function() { $('#coveo-search-block-form').toggleClass('open');});
     $(context).find('#insights-search-touch-button').click(function() { $('#insights-search-block-form').toggleClass('open'); $(this).toggleClass('open');});
     $(context).find('#block-system-main-menu li').click(function() { $(this).toggleClass('open');});
     $(context).find('#block-menu-menu-executive-education-mega-me li').click(function() { $(this).toggleClass('open');});
