423d7dcc5d431bb052b992468095457f3e9abaab
Greg Mercer (GBear) <gmercer@stanford.edu>
2018-07-25 11:18:19 -0700

    Revert "WP-8458 fixes to the interaction of the megamenu. alignment of the search box and open close boxes."
    This reverts commit c81a6261b19548c71f0cd719b74d8fa0b3230948.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -78,19 +78,12 @@
     <div id="header-wrapper"><div id="header"><div class="section clearfix">
           <a id="search-touch-button"></a>
 
-          <?php if ($logo): ?>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-                  <img src="/<?php print drupal_get_path('theme', 'gsb_theme'); ?>/images/logo-print.jpg" alt="Home">
-              </a>
-          <?php endif; ?>
-
           <?php if ($page['navigation']): ?>
             <div id="nav-touch-wrapper">
               <a id="nav-touch-button"></a>
               <div id="navigation"><div class="section">
                   <?php print render($page['navigation']); ?>
                   <?php print render($global_search); ?>
-                  <div id="search-close"></div>
                   <div id="menu-main-footer">
                     <?php
                     $footer_1 = module_invoke('menu', 'block_view', 'menu-footer-1');
@@ -101,6 +94,16 @@
                   </div>
                 </div></div> <!-- /.section, /#navigation -->
             </div> <!-- /#nav-touch-wrapper-->
+          <?php endif; ?>
+
+          <?php if ($site_slogan): ?>
+            <div id="site-slogan"><?php print $site_slogan; ?></div>
+          <?php endif; ?>
+
+          <?php if ($logo): ?>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+              <img src="/<?php print drupal_get_path('theme', 'gsb_theme'); ?>/images/logo-print.jpg" alt="Home">
+            </a>
           <?php endif; ?>
 
           <?php if ($page['header']): ?>
