98001c60dad7024239c6039ffeb518e7db54fd91
Jennifer Breese Kauth <jbreese@stanford.edu>
2018-12-05 15:01:08 -0800

    WP-8780 Fixes to the Responsive Section menus.

--- js/custom.js
+++ js/custom.js
@@ -659,6 +659,24 @@
         };
     });
     $(function(){
-      $("#sidebar .expanded.active-trail.open.collapsed").removeClass("collapsed");
+        var width = $(window).width();
+        if (width > 1199) {
+          //desktop only functionality
+        };
+        if (width <= 1199) {
+            //phone/tablet only functionality
+            $("#sidebar").find(".content > .menu li.expanded > a")
+                .replaceWith(function(){
+                  return $("<span class='sub-menus'>" + $(this).html() + "</span>");
+                });
+            $("#sidebar").find(".content > .menu > li.expanded").addClass("top-level");
+            $("#sidebar").find(".menu li.expanded")
+                .click(function(e) {
+                    if ($(this).hasClass("expanded")) {
+                        $(this).toggleClass("open");
+                        $(".top-level").not($(this)).removeClass('open');
+                    }
+                });
+        };
     });
 }(jQuery));
--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -15,17 +15,6 @@ Drupal.behaviors.responsiveMenu = {
     $(context).find('#block-menu-menu-executive-education-mega-me li').click(function() { $(this).toggleClass('open');});
     $(context).find('#block-menu-menu-mega-menu-seed li').click(function() { $(this).toggleClass('open');});
     $(context).find('#block-menu-section-27256').click(function() { $(this).toggleClass('open');});
-    $(context).find('#sidebar .menu li.expanded.active-trail').addClass('open');
-    $(context).find('#sidebar .menu li.expanded').addClass('collapsed')
-        .click(function (e){
-          if ($(this).hasClass('collapsed')) {
-              $(this).removeClass('collapsed').addClass('open');
-              e.stopPropagation();
-              e.preventDefault();
-          } else if ($(this).hasClass('open')) {
-              $(this).removeClass('open').addClass('collapsed');
-          }
-      });
   }
 };
 
