06028e3d358825b094916f36253e8054f0caf065
justinriddiough <justinriddiough@stanford.edu>
2019-02-21 17:50:54 -0800

    WP-8907 cleanup formatting for readability

--- js/search.js
+++ js/search.js
@@ -1,37 +1,39 @@
 (function ($) {
 
-  /**
-   * Add javascript functionality to the search menu.
-   */
-  Drupal.behaviors.gsb_search_box = {
-    attach: function () {
-      $('#navigation').addClass('search-closed');
-      $('.global-search input[type="submit"]').click(function(e) {
-        if ($('#navigation').hasClass('search-closed')) {
-          $('#navigation').removeClass('search-closed');
-          $('#navigation').addClass('search-open');
-          $('.global-search input[name="search_keys"]').focus();
-          e.preventDefault();
-        }
-      });
-        $('#page').addClass('modal-closed');
-        $('.global-search input[type="submit"]').click(function(e) {
-            if ($('#page').hasClass('modal-closed')) {
-                $('#page').removeClass('modal-closed');
-                $('#page').addClass('modal-open');
-                $('.global-search input[name="search_keys"]').focus();
-                e.preventDefault();
-            }
-        });
-        $('#search-close').click(function(e){ $('#page').removeClass('modal-open').addClass('modal-closed')});
+    /**
+     * Add javascript functionality to the search menu.
+     */
+    Drupal.behaviors.gsb_search_box = {
+        attach: function () {
+            $('#navigation').addClass('search-closed');
+            $('.global-search input[type="submit"]').click(function (e) {
+                if ($('#navigation').hasClass('search-closed')) {
+                    $('#navigation').removeClass('search-closed');
+                    $('#navigation').addClass('search-open');
+                    $('.global-search input[name="search_keys"]').focus();
+                    e.preventDefault();
+                }
+            });
+            $('#page').addClass('modal-closed');
+            $('.global-search input[type="submit"]').click(function (e) {
+                if ($('#page').hasClass('modal-closed')) {
+                    $('#page').removeClass('modal-closed');
+                    $('#page').addClass('modal-open');
+                    $('.global-search input[name="search_keys"]').focus();
+                    e.preventDefault();
+                }
+            });
+            $('#search-close').click(function (e) {
+                $('#page').removeClass('modal-open').addClass('modal-closed')
+            });
 
-        $(document).keydown(function(event) {
-            if (event.keyCode == 27) {
-                $('#page').removeClass('modal-open');
-                $('#navigation').removeClass('search-open');
-            }
-        });
-    }
-  };
+            $(document).keydown(function (event) {
+                if (event.keyCode == 27) {
+                    $('#page').removeClass('modal-open');
+                    $('#navigation').removeClass('search-open');
+                }
+            });
+        }
+    };
 
 }(jQuery));
